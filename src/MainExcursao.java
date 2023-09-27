88b1b29f223d0c099bad00476fcb2798c78ea2fc
vtrnduda <vtrnduda@gmail.com>
2023-09-27 14:13:41 -0300

    Testing carregar()

--- src/MainExcursao.java
+++ src/MainExcursao.java
@@ -2,7 +2,18 @@
 public class MainExcursao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+			Excursao e = new Excursao(1);
+			e.carregar();
+			System.out.println(e);
+			e.cancelarReserva("444");
+			System.out.println(e);
+
+		} catch(Exception er) {
+			System.out.println(er.getMessage());
+			
+		}
+
 
 	}
 
